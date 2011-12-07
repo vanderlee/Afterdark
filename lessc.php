d19f3c45c149a2b8994cc55b9adfd0ca25c68a25
Martijn van der Lee <martijn@vanderlee.com>
2011-12-07 22:39:06 +0100

    Using LessPHP to compile CSS

---
+++ lessc.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Content-Type: text/css');
+
+require 'lessc.inc.php';
+
+try {
+	$out = str_replace('.less', '', $_SERVER['QUERY_STRING']);
+	@unlink($out);
+    lessc::ccompile($_SERVER['QUERY_STRING'], $out);
+
+	$header = "/*\n"
+			. " * AfterDark jQueryUI theme\n"
+			. " * Copyright ".date('Y')." Martijn W. van der Lee\n"
+			. " * Licensed under Creative Commons Attribution-Share Alike 3.0\n"
+			. " * Build ".date('Y-m-d H:i:s')."\n"
+			. " */\n"
+			;
+	file_put_contents($out, $header . file_get_contents($out));
+} catch (exception $ex) {
+    exit('lessc fatal error:<br />'.$ex->getMessage());
+}
+
+readfile($out);
+
+?>
